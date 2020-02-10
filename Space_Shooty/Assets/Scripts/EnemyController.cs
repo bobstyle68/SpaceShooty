962ad0b9233a3c01d01fe165213a9462ac6b8e50
bobstyle68 <60360117+bobstyle68@users.noreply.github.com>
2020-02-10 10:47:13 +0000

    SpaceShootyV2
    PewPewPew

---
+++ Space_Shooty/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speed = 5;
+    Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+
+        body = GetComponent<Rigidbody2D>();
+        body = new Vector2();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
